83273bccd053736c1f610aa3f9c8bf230def4f4b
RodBrowning <rodrigo.browning@gmail.com>
2022-10-02 06:11:52 -0300

    Fix price display tests

--- src/components/display/price/index.test.js
+++ src/components/display/price/index.test.js
@@ -2,17 +2,17 @@ import { render, screen } from '@testing-library/react';
 import PriceDisplay from './index';
 
 describe('Price display', () => {
-    test('Should display $15.00 / $18.00', () => {
-      render(<PriceDisplay price={15} oldPrice={18} />);
+    test('Should display £15.00 / £18.00', () => {
+      render(<PriceDisplay price={15} oldPrice={18} currencyInfo={{locale: "en-GB", currencyCode: "GBP"}} />);
         
-      const priceWithOldPrice = screen.getByRole('heading', {level: 4, name: '$15.00 / $18.00'});
+      const priceWithOldPrice = screen.getByRole('heading', {level: 4, name: '£15.00 / £18.00'});
       expect(priceWithOldPrice).toBeInTheDocument();
     });
 
-    test('Should display $15.00', () => {
-      render(<PriceDisplay price={15} />);
+    test('Should display £15.00', () => {
+      render(<PriceDisplay price={15} currencyInfo={{locale: "en-GB", currencyCode: "GBP"}} />);
         
-      const price = screen.getByRole('heading', {level: 4, name: '$15.00'});
+      const price = screen.getByRole('heading', {level: 4, name: '£15.00'});
       expect(price).toBeInTheDocument();
     });
 })
