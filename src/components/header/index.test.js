38836627d14423a4d0d5a6673a1a26e7817e12c3
RodBrowning <rodrigo.browning@gmail.com>
2022-10-04 01:31:36 -0300

    Test Header component

--- src/components/header/index.test.js
+++ src/components/header/index.test.js
@@ -1,22 +1,123 @@
-import {render, screen} from '@testing-library/react';
-import '@testing-library/jest-dom';
+import {screen} from '@testing-library/react';
+import { renderWithProviders } from '../../utils/test-utils';
+import { fetchProducts } from '../../features/products/productsSlice';
+import { addToCart } from '../../features/cart/cartSlice';
 import Header from './index';
-import {BrowserRouter} from 'react-router-dom';
 
 describe('Header', () => {
-    test('Should render all nav links', async () => {
-      render(<Header />, {wrapper: BrowserRouter})
-        
-        const logoText = screen.getByRole('heading', {level: 2, name: "90's Shop"});
-        expect(logoText).toBeInTheDocument();
+  test('Should render all nav links', async () => {
+    renderWithProviders(<Header />)
+      
+      const logoText = screen.getByRole('heading', {level: 2, name: "90's Shop"});
+      expect(logoText).toBeInTheDocument();
 
-        const logoLink = screen.getByRole('link', {name: "90's Shop"});
-        expect(logoLink).toBeInTheDocument();
+      const logoLink = screen.getByRole('link', {name: "90's Shop"});
+      expect(logoLink).toBeInTheDocument();
 
-        const homeLink = screen.getByRole('link', {name: "Home"});
-        expect(homeLink).toBeInTheDocument();
+      const homeLink = screen.getByRole('link', {name: "Home"});
+      expect(homeLink).toBeInTheDocument();
 
-        const cartLink = screen.getAllByRole('link');
-        expect(cartLink[2]).toBeInTheDocument();
-    });
-})
+      const cartLink = screen.getAllByRole('link');
+      expect(cartLink[2]).toBeInTheDocument();
+  });
+  
+  test('Should render cart number indicator with 1 products', async () => {
+    const product = {
+      id: 100,
+      brand: "brandA",
+      title: "titleA",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis porro.",
+      freeShipping: true,
+      newest: true,
+      price: {
+          value: 15,
+          oldValue: 20,
+          shipping: 2,
+          currencyInfo: {
+              locale: "en-GB",
+              currencyCode: "GBP"
+          }
+      },
+      imageURL: "http://localhost:8080/images/TomyPocketGames.jpg",
+      imageAlt: "Lorem ipsum dolor sit amet consectetur.",
+      availableQuantity: 3,
+      quantity: 2
+    };
+    const {store} = renderWithProviders(<Header />)
+    await store.dispatch(fetchProducts());
+    await store.dispatch(addToCart({...product}));
+    
+    const numberIndicator = await screen.findByTestId('num-itens');
+    expect(numberIndicator).toBeInTheDocument();
+    expect(numberIndicator.innerHTML).toBe("1");
+  });
+  
+  test('Should render cart number indicator above 100 products', async () => {
+    const product = {
+      id: 100,
+      brand: "brandA",
+      title: "titleA",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis porro.",
+      freeShipping: true,
+      newest: true,
+      price: {
+          value: 15,
+          oldValue: 20,
+          shipping: 2,
+          currencyInfo: {
+              locale: "en-GB",
+              currencyCode: "GBP"
+          }
+      },
+      imageURL: "http://localhost:8080/images/TomyPocketGames.jpg",
+      imageAlt: "Lorem ipsum dolor sit amet consectetur.",
+      availableQuantity: 3,
+      quantity: 2
+    };
+    const {store} = renderWithProviders(<Header />)
+    await store.dispatch(fetchProducts());
+    await store.dispatch(addToCart({...product}));
+    
+    const numberIndicator = await screen.findByTestId('num-itens');
+    expect(numberIndicator).toBeInTheDocument();
+
+    for (let index = 0; index < 100; index++) {
+      await store.dispatch(addToCart({...product}));
+    }
+    expect(numberIndicator.innerHTML).toBe("+99");
+  });
+  
+  test('Should animate cart number indicator', async () => {
+    const product = {
+      id: 100,
+      brand: "brandA",
+      title: "titleA",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis consectetur adipisicing elit. Quidem blanditiis porro.",
+      freeShipping: true,
+      newest: true,
+      price: {
+          value: 15,
+          oldValue: 20,
+          shipping: 2,
+          currencyInfo: {
+              locale: "en-GB",
+              currencyCode: "GBP"
+          }
+      },
+      imageURL: "http://localhost:8080/images/TomyPocketGames.jpg",
+      imageAlt: "Lorem ipsum dolor sit amet consectetur.",
+      availableQuantity: 3,
+      quantity: 2
+    };
+    const {store} = renderWithProviders(<Header />)
+    await store.dispatch(fetchProducts());
+    await store.dispatch(addToCart({...product}));
+    
+    const numberIndicator = await screen.findByTestId('num-itens');
+    expect(numberIndicator).toBeInTheDocument();
+    expect(numberIndicator).toHaveClass("animate");
+    expect(numberIndicator.innerHTML).toBe("1");
+    await new Promise((r) => setTimeout(r, 2000));
+    expect(numberIndicator).not.toHaveClass("animate");
+  });
+});
