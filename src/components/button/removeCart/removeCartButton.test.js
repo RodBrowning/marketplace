acdb0843e17d9014e580fa0123ecdc16c01fac07
RodBrowning <rodrigo.browning@gmail.com>
2022-10-04 01:28:23 -0300

    Test RemoveCartButton component

--- src/components/button/removeCart/removeCartButton.test.js
+++ src/components/button/removeCart/removeCartButton.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import RemoveCartButton from './removeCartButton';
 
-test('Should render checkout button', () => {
-  render(<RemoveCartButton />);
-    const removeCartButton = screen.getByRole('button');
-    expect(removeCartButton).toBeInTheDocument();
+describe('Remove cart button',()=>{
+  test('Should render successfully', () => {
+    render(<RemoveCartButton />);
+      const removeCartButton = screen.getByRole('button');
+      expect(removeCartButton).toBeInTheDocument();
+  });
+  
+  test('Should disabled', async () => {
+    render(<RemoveCartButton disabled={true} />);
+      expect(screen.getByRole('button')).toHaveClass('disabled');
+  });
+  
+  test('Should call function hadler when click', async () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    user.setup();
+    render(<RemoveCartButton buttonAction={()=> alert('ok')}/>);
+      const buttonElement = screen.getByRole('button');
+      expect(buttonElement).toBeInTheDocument();
+  
+      await user.click(buttonElement);
+      expect(window.alert).toBeCalled();
+  });
 });
