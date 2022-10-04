cab6cf631e3610682eb475e19aedacb91240d612
RodBrowning <rodrigo.browning@gmail.com>
2022-10-04 01:27:57 -0300

    Test CheckoutButton component

--- src/components/button/checkout/checkout.test.js
+++ src/components/button/checkout/checkout.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import CheckoutButton from './checkout';
 
-test('Should render checkout button', () => {
-  render(<CheckoutButton />);
-    const checkoutButton = screen.getByText(/Checkout/i);
-    expect(checkoutButton).toBeInTheDocument();
+describe('Add checkout button',()=>{
+  test('Should render checkout button', () => {
+    render(<CheckoutButton />);
+    const checkoutButton = screen.getByRole('button', {name: /Checkout/i});
+     expect(checkoutButton).toBeInTheDocument();
+  });
+  
+  test('Should display checkout alert', async () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    user.setup();
+    render(<CheckoutButton />);
+      const checkoutButton = screen.getByRole('button', {name: /Checkout/i});
+      await user.click(checkoutButton);
+      expect(window.alert).toBeCalled();
+  });
 });
