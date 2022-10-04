2f14c43abb426ee586bd411d8a0d2be2ec1bd254
RodBrowning <rodrigo.browning@gmail.com>
2022-10-04 01:27:40 -0300

    Test AddCartButton component

--- src/components/button/addCart/addCartButton.test.js
+++ src/components/button/addCart/addCartButton.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import AddCartButton from './addCartButton';
+import user from '@testing-library/user-event';
 
-test('Should render add cart button', () => {
-  render(<AddCartButton />);
-    const buttonElement = screen.getByText(/Add to cart/i);
-    expect(buttonElement).toBeInTheDocument();
+describe('Add cart button',()=>{
+  test('Should render add cart button', () => {
+    render(<AddCartButton />);
+      const buttonElement = screen.getByText(/Add to cart/i);
+      expect(buttonElement).toBeInTheDocument();
+  });
+  
+  test('Should render disabled cart button', async () => {
+    render(<AddCartButton disabled={true} />);
+      expect(screen.getByRole('button')).toHaveClass('disabled');
+  });
+  
+  test('Click test', async () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    user.setup();
+    render(<AddCartButton buttonAction={()=> alert('ok')}/>);
+      const buttonElement = screen.getByText(/Add to cart/i);
+      expect(buttonElement).toBeInTheDocument();
+  
+      await user.click(buttonElement);
+      expect(window.alert).toBeCalled();
+  });
 });
