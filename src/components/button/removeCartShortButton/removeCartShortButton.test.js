c06cd09bbb0541db54443b63eb4048d35475706a
RodBrowning <rodrigo.browning@gmail.com>
2022-10-04 01:29:26 -0300

    Test AddCartShortButton component

--- src/components/button/removeCartShortButton/removeCartShortButton.test.js
+++ src/components/button/removeCartShortButton/removeCartShortButton.test.js
@@ -1,9 +1,22 @@
 import { render, screen } from '@testing-library/react';
-
+import user from '@testing-library/user-event';
 import RemoveCartShortButton from './removeCartShortButton';
 
-test('Should render checkout button', () => {
-  render(<RemoveCartShortButton />);
-    const removeCartShortButton = screen.getByRole('button');
-    expect(removeCartShortButton).toBeInTheDocument();
+describe('Remove cart button - short version',()=>{
+  test('Should render successfully', () => {
+    render(<RemoveCartShortButton />);
+      const removeCartShortButton = screen.getByRole('button');
+      expect(removeCartShortButton).toBeInTheDocument();
+  });
+  
+  test('Should call function hadler when click', async () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    user.setup();
+    render(<RemoveCartShortButton buttonAction={()=> alert('ok')}/>);
+      const buttonElement = screen.getByRole('button');
+      expect(buttonElement).toBeInTheDocument();
+  
+      await user.click(buttonElement);
+      expect(window.alert).toBeCalled();
+  });
 });
