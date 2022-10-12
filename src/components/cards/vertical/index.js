ced4da29c2ad06e013259a64cd13419617e15317
RodBrowning <rodrigo.browning@gmail.com>
2022-10-12 03:02:40 -0300

    Code review - remove hooks

--- src/components/cards/vertical/index.js
+++ src/components/cards/vertical/index.js
@@ -2,7 +2,6 @@ import './style.scss';
 import './style-mobile.scss';
 
 import { getPercentage, truncateString } from '../../../utils/utils';
-import { useEffect, useState } from 'react';
 
 import AddCartButton from '../../button/addCart/addCartButton';
 import DiscountDisplay from '../../display/discount';
@@ -25,17 +24,11 @@ const VerticalCard = ({
         handleRemoveFromCart, 
         isInTheCart
     }) => {
-    const [discount, setDiscount] = useState(0)
-    useEffect(() => {
-        if (price && oldPrice !== undefined) {
-            setDiscount(getPercentage(price, oldPrice));
-        }
-    }, [oldPrice, price]); 
     
     return (
         <div id="vertical-card"  onClick={()=>{goToProductPageHandler()}} data-testid="vertical-card">
             <div className="card-img">
-                <DiscountDisplay discount={discount} />
+                <DiscountDisplay discount={getPercentage(price, oldPrice)} />
                 <img src={image} alt={imgAlt} />
             </div>
             <div className="card-body">
