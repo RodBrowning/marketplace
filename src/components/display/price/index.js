b0da2f3cce3986532a9b5a2f253a32ec2f841770
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-11-20 13:39:54 -0300

    Fix: Duplicated id elements from price display

--- src/components/display/price/index.js
+++ src/components/display/price/index.js
@@ -5,7 +5,7 @@ import { numToCurrency } from '../../../utils/utils';
 
 const PriceDisplay = ({price, oldPrice, currencyInfo }) => {
     return (
-        <h4 id="price-display"><span>{numToCurrency(price, currencyInfo)}</span>{oldPrice && <>/<span className='oldPrice'>{numToCurrency(oldPrice, currencyInfo)}</span></>}</h4>
+        <h4 className="price-display"><span>{numToCurrency(price, currencyInfo)}</span>{oldPrice && <>/<span className='oldPrice'>{numToCurrency(oldPrice, currencyInfo)}</span></>}</h4>
     );
 }
 
